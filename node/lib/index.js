c2b1ebf768a78451e253dcb8f1d65a12fd421648
Nathan Kamm <nathan.kamm@oath.com>
2018-07-21 13:03:01 -0700

    docker + index

--- node/lib/index.js
+++ node/lib/index.js
@@ -1,38 +1,54 @@
 const fs = require('fs');
 const cmd = require('node-cmd');
 
+var map = {
+  'bash': {
+    'container': 'tickticktick-bash:latest',
+    'filename': 'script.sh'
+  },
+  'cpp': {
+    'container': 'tickticktick-cpp:latest',
+    'filename': 'main.cpp'
+  }
+}
+
 const dockerMap = (epoch, key) => {
 
   const dockerCLI = `docker run \
                     -v ${__dirname}/${epoch}:/scripts \
+                    -v ${__dirname}/docker/${key}:/internal/run \
                     -w /scripts \
                     -t`
 
   const pipeOutput = `> ${__dirname}/${epoch}/output.txt`
-
-  const map = {
-    'bash': {
-      'container': 'ubuntu',
-      'script': 'bash script.sh'
-    }
-  }
-  return `${dockerCLI} ${map[key]['container']} ${map[key]['script']} ${pipeOutput}`
+  // console.log(`${dockerCLI} ${map[key]['container']} bash ${__dirname}/docker/${key}/run.sh ${pipeOutput}`)
+  return `${dockerCLI} ${map[key]['container']} bash /internal/run/run.sh ${pipeOutput}`
 }
 
 const mockFile = () => {
   return `#!/usr/bin/env bash \n ls -al \n echo "start" \n sleep 5 \n echo "Hello, World!"`
 }
 
-const runCode = (interpreter = 'bash', file=mockFile()) => {
+const mockCPP = () => {
+  return `#include <iostream>
+
+  using namespace std;
+
+  auto main() -> int {
+    cout << " Hello From Mock " << endl;
+  }`
+}
+
+const runCode = (language = 'cpp', file=mockCPP()) => {
   return new Promise((resolve, reject) => {
     const epoch = new Date().getTime()
     const dir = `${__dirname}/${epoch}`
 
     fs.mkdirSync(dir)
-    fs.writeFile(dir + '/script.sh', file, (err) => {
+    fs.writeFile(`${dir}/${map[language]['filename']}`, file, (err) => {
       cmd.get(`
         cd ${epoch}
-        ${dockerMap(epoch, interpreter)}
+        ${dockerMap(epoch, language)}
       `, (err, data) => {
         fs.readFile(dir + '/output.txt', 'utf8', (err, data) => {
           cmd.run(`rm -r ${dir}`)
@@ -44,6 +60,7 @@ const runCode = (interpreter = 'bash', file=mockFile()) => {
   })
 }
 
-module.exports = {
-  runCode
-}
+runCode().then(console.log).catch(console.log)
+// module.exports = {
+//   runCode
+// }
